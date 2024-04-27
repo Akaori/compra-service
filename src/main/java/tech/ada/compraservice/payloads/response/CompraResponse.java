9e9989608a70483374dcfff81f883e539aef2bd4
Akaori <akiyamaanaluiza@gmail.com>
2024-04-27 18:55:02 -0300

    feat: add response payload

---
+++ src/main/java/tech/ada/compraservice/payloads/response/CompraResponse.java
@@ -0,0 +1,8 @@
+package tech.ada.compraservice.payloads.response;
+
+public class CompraResponse {
+    private String compraId;
+    private String psgamentoId;
+    private String envioId;
+    private String status;
+}
