28e9683ae951d5fb0c770ca84265216914e383e4
Akaori <akiyamaanaluiza@gmail.com>
2024-04-28 18:02:15 -0300

    fix: change webclient to feign

--- src/main/java/tech/ada/compraservice/CompraApplication.java
+++ src/main/java/tech/ada/compraservice/CompraApplication.java
@@ -1,9 +1,14 @@
 package tech.ada.compraservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
+@EnableFeignClients(basePackages = "tech.ada.compraservice.client")
 public class CompraApplication {
 
 	public static void main(String[] args) {
--- src/main/java/tech/ada/compraservice/client/PagamentoClient.java
+++ src/main/java/tech/ada/compraservice/client/PagamentoClient.java
@@ -1,10 +1,13 @@
 package tech.ada.compraservice.client;
 
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.service.annotation.PostExchange;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import tech.ada.compraservice.payloads.request.PagamentoRequest;
 
+@FeignClient(name = "pagamentoClient", url = "${negocio.pagamento.url}")
 public interface PagamentoClient {
-    @PostExchange(value = "/")
+    @RequestMapping(method = RequestMethod.POST, value = "/", consumes = "application/json")
     void realizarPagamento(@RequestBody PagamentoRequest pagamento);
 }
