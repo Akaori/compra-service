c1e5e34410df5beb8f23de903622f7788d17d060
Akaori <akiyamaanaluiza@gmail.com>
2024-04-27 22:17:31 -0300

    feat: add compra status

--- src/main/java/tech/ada/compraservice/model/Compra.java
+++ src/main/java/tech/ada/compraservice/model/Compra.java
@@ -2,6 +2,7 @@ package tech.ada.compraservice.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import tech.ada.compraservice.model.enums.StatusCompra;
 
 import java.util.UUID;
 
@@ -13,4 +14,6 @@ public class Compra {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
     private UUID id;
+
+    private StatusCompra status;
 }
---
+++ src/main/java/tech/ada/compraservice/model/enums/StatusCompra.java
@@ -0,0 +1,5 @@
+package tech.ada.compraservice.model.enums;
+
+public enum StatusCompra {
+    INICIADA, AGUARDANDO_ENVIO, AGUARDANDO_ENTREGA, CONCLUIDA, ERRO
+}
